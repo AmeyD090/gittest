f02375de7fa5e95cd51a0e555dfc9beaa95d6445
ameydeshmukh <ameydeshmukh090@gmail.com>
2024-12-08 07:18:55 +0000

    java code

--- app.py
+++ app.py
@@ -1,2 +1,23 @@
-print("DEADPOOL & WOLVERIN")
+package com.docker.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+
+public class DockerDemoApplication {
+
+	@GetMapping("/message")
+	public String getMessage(){
+		return"Hello docker";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDemoApplication.class, args);
+	}
+
+}
 
