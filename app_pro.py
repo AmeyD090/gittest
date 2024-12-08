70344001c6814adca7b13ad01de4d1ce520c5222
local_pc <local_pc@gmail.com>
2024-12-08 14:24:01 +0530

    remove file and change in app_pro

--- app_pro.py
+++ app_pro.py
@@ -1,13 +1,1 @@
-package com.docker.demo;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DockerDemoApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+print("apple")
